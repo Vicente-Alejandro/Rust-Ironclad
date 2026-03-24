87861232ea73e27e1360b1ee607b80e53088dbee
Vicente Alejandro <realumanin@gmail.com>
2026-03-24 00:55:15 -0300

    New jobs info function

--- src/queue/mod.rs
+++ src/queue/mod.rs
@@ -2,6 +2,6 @@ mod job;
 mod manager;
 mod worker;
 
-pub use job::{Job, JobType, JobPayload, JobStatus};
+pub use job::{Job, JobType, JobPayload};
 pub use manager::QueueManager;
 pub use worker::Worker;
